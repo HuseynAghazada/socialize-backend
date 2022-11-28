df6ea015bc93756b015691e9698608898ca8350e
huseynAgazade <huseyn.aghazada.std@bhos.edu.az>
2022-11-29 01:07:42 +0400

    sd

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ const app = express();
 // middlewares
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', authRoute);
