63ed97df710964829a0f9d1e807cbddee5655adc
huseynAgazade <huseyn.aghazada.std@bhos.edu.az>
2022-11-29 00:47:17 +0400

    environment integration

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.BASE_URL,
     credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
